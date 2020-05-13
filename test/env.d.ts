f6e10a74cc133f2770fdcdb294f5450b28eaed57
Yagi Sumiya <goatship4@yahoo.co.jp>
2020-05-14 03:02:58 +0900

    Add type definition of test environment

---
+++ test/env.d.ts
@@ -0,0 +1,23 @@
+declare function setEnv(key: string, value: string): void
+declare function deleteEnv(key: string): void
+declare function mkdir(path: string): void
+declare function exists(path: string): boolean
+declare function rimraf(dir_path: string): void
+declare function copyFile(src: string, dest: string): void
+
+type CommandCallback = (err: Error | undefined, data: any) => void
+interface GroongaClient {
+  command(command: string, options: object, callback: CommandCallback): void
+  command(command: string, callback: CommandCallback): void
+}
+type SetupConfig = {
+  db_path: string
+}
+interface TestEnv {
+  client: GroongaClient
+  config: SetupConfig
+}
+
+declare function setup(config: SetupConfig): Promise<TestEnv>
+declare function teardown(env: TestEnv): Promise<void>
+declare const context: any
